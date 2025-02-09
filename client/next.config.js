12e4766e06346abf6524b4f624491cde9c6cecb8
AmineNaboulsi <naboulsiiamine@gmail.com>
2025-02-09 17:49:25 +0100

    chor: instead of vercel file config i declare the missing config route on next config

--- client/next.config.js
+++ client/next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'standalone',
-    experimental: {
-      appDir: true
-    }
+    async rewrites() {
+      return [
+        {
+          source: '/:path*',
+          destination: '/',
+        },
+      ]
+    },
   }
   
 module.exports = nextConfig
